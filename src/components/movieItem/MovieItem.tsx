4c39f75e120386f068d12fc6e341592e95384f65
olegmakeienko <molva83@gmail.com>
2024-06-01 00:32:59 +0200

    MovieItems changed, have link to MoviePage

--- src/components/movieItem/MovieItem.tsx
+++ src/components/movieItem/MovieItem.tsx
@@ -1,14 +1,16 @@
-import './movieItem.css'
-import {Movie} from "../../store";
+import { Link } from 'react-router-dom';
+import {Movie} from "../../models/model.ts";
 
 interface MovieItemProps {
     movie: Movie;
 }
 
-function MovieItem ({ movie }: MovieItemProps) {
+function MovieItem ({ movie } : MovieItemProps) {
     return (
         <div className="movie-item">
-            <h2>{movie.title}</h2>
+            <h2>
+                <Link to={`/movie/${movie.imdbid}`}>{movie.title}</Link>
+            </h2>
             <img src={movie.poster} alt={movie.title} />
             <p>IMDB ID: {movie.imdbid}</p>
             <a href={movie.trailer_link} target="_blank" rel="noopener noreferrer">
