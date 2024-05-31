bf2a671c49441b711f64036c6949655eeda7a62a
olegmakeienko <molva83@gmail.com>
2024-06-01 00:37:38 +0200

    HomaPage created

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,21 @@
+import {Movie} from "../models/model.ts";
+import MovieItem from "../components/movieItem/MovieItem.tsx";
+
+interface HomePageProps {
+    movies: Movie[];
+}
+
+function HomePage ({ movies } : HomePageProps) {
+    return (
+        <div className="home-page">
+            <h1>Movie List</h1>
+            <div className="movie-list">
+                {movies.map(movie => (
+                    <MovieItem key={movie.imdbid} movie={movie} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
