18f61b68d398bb515fb3c67ea8416d353a081caf
olegmakeienko <molva83@gmail.com>
2024-06-02 20:53:50 +0200

    smile added to button in Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,18 @@
 import './header.css'
 import {Link} from "react-router-dom";
+import RegisterIcon from "../../assets/1432299223.png"
 
 function Header() {
     return (
         <header className='header-page'>
             <nav className='header'>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/login">Login</Link></li>
-                    <li><Link to="/register">Register</Link></li>
+                    <li><Link to="/" className="button">Home</Link></li>
+                    <li><Link to="/login" className="button">Login</Link></li>
+                    <li><Link to="/register" className="button">
+                        <img src={RegisterIcon} alt="Register Icon" />
+                        Register
+                    </Link></li>
                 </ul>
             </nav>
 
