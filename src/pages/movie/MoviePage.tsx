6d6b4d4c51ea62b9146e8a3b730ba7c1d8311771
olegmakeienko <molva83@gmail.com>
2024-06-02 16:27:03 +0200

    favorite button use toggleFavorite from store

--- src/pages/movie/MoviePage.tsx
+++ src/pages/movie/MoviePage.tsx
@@ -4,22 +4,29 @@ import {useStore} from "../../store/store.ts";
 
 function MoviePage() {
     const { imdbid } = useParams<{ imdbid: string }>();
-    const { movies } = useStore();
+    const { movies, toggleFavorite } = useStore();
     const movie = movies.find(movie => movie.imdbid === imdbid);
 
     if (!movie) {
         return <div>Movie not found</div>;
     }
 
+    const handleFavoriteClick = () => {
+        toggleFavorite(movie.imdbid);
+    };
+
     return (
         <div className="movie-page">
             <h1>{movie.title}</h1>
-            <img src={movie.poster} alt={movie.title} />
+            <img src={movie.poster} alt={movie.title}/>
             <p>IMDB ID: {movie.imdbid}</p>
             <a href={movie.trailer_link} target="_blank" rel="noopener noreferrer">
                 Watch Trailer
             </a>
             <p>Favorite: {movie.is_favorite ? "Yes" : "No"}</p>
+            <button onClick={ handleFavoriteClick }>
+                {movie.is_favorite ? "Unfavorite" : "Add to Favorite"}
+            </button>
         </div>
     );
 };
