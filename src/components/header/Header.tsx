45b8f85028d9ea6b8db300d7f251b7fd3c35541d
olegmakeienko <molva83@gmail.com>
2024-06-03 17:39:35 +0200

    link to My Profile

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,24 @@
 import './header.css'
-import {Link} from "react-router-dom";
-import RegisterIcon from "../../assets/1432299223.png"
+import { Link } from "react-router-dom";
+import RegisterIcon from "../../assets/1432299223.png";
+import { useStore } from "../../store/store.ts";
 
 function Header() {
+    const { isLoggedIn } = useStore();
+
     return (
         <header className='header-page'>
             <nav className='header'>
                 <ul>
                     <li><Link to="/" className="button">Home</Link></li>
-                    <li><Link to="/login" className="button">Login</Link></li>
-                    <li><Link to="/register" className="button">
-                        <img src={RegisterIcon} alt="RegisterPage Icon" />
-                        RegisterPage
-                    </Link></li>
+                    {!isLoggedIn && <li><Link to="/login" className="button">Login</Link></li>}
+                    {!isLoggedIn && <li><Link to="/register" className="button">
+                        <img src={RegisterIcon} alt="Register Icon" />
+                        Register
+                    </Link></li>}
+                    {isLoggedIn && <li><Link to="/profile" className="button">My Profile</Link></li>}
                 </ul>
             </nav>
-
         </header>
     );
 }
