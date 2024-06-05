6c80625a80a276123b4d978e113a54a8bd57f652
olegmakeienko <molva83@gmail.com>
2024-06-05 23:40:59 +0200

    Header: control isLoggetIn,

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,22 +1,41 @@
-import './header.css'
-import { Link } from "react-router-dom";
-import RegisterIcon from "../../assets/1432299223.png";
-import { useStore } from "../../store/store.ts";
+import './header.css';
+import { Link } from 'react-router-dom';
+import RegisterIcon from '../../assets/1432299223.png';
+import { useStore } from '../../store/store.ts';
+import {useEffect} from "react";
 
 function Header() {
     const { isLoggedIn } = useStore();
+
+    useEffect(() => {
+        console.log('isLoggedIn:', isLoggedIn);
+    }, [isLoggedIn]);
 
     return (
         <header className='header-page'>
             <nav className='header'>
                 <ul>
-                    <li><Link to="/" className="button">Home</Link></li>
-                    {!isLoggedIn && <li><Link to="/login" className="button">Login</Link></li>}
-                    {!isLoggedIn && <li><Link to="/register" className="button">
-                        <img src={RegisterIcon} alt="Register Icon" />
-                        Register
-                    </Link></li>}
-                    {isLoggedIn && <li><Link to="/profile" className="button">My Profile</Link></li>}
+                    {isLoggedIn ? (
+                        <li>
+                            <Link to='/' className='button'>
+                                This is your movie storage
+                            </Link>
+                        </li>
+                    ) : (
+                        <>
+                            <li>
+                                <Link to='/login' className='button'>
+                                    My Movie Storage
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/register' className='button'>
+                                    <img src={RegisterIcon} alt='Register Icon' />
+                                    Register
+                                </Link>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </nav>
         </header>
