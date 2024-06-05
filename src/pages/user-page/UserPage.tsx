d07f777b9aca12264e24666c5cc63d97bc2c46ad
olegmakeienko <molva83@gmail.com>
2024-06-05 23:42:54 +0200

    userpage: trailer, delete function, favorite icon with onClick function

--- src/pages/user-page/UserPage.tsx
+++ src/pages/user-page/UserPage.tsx
@@ -1,16 +1,25 @@
 import { useStore } from "../../store/store.ts";
-import { useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import goldStar from "../../assets/gold-star.svg";
+import grayStar from "../../assets/gray-star.svg";
+import basket from "../../assets/korg.png"
 
 import "./userPage.css"
 
 function UserPage() {
-    const { user, movies, addMovie, deleteMovie, setUser, apiKey } = useStore();
+    const { user, movies, addMovie, deleteMovie, setUser, apiKey, toggleFavorite } = useStore();
     const navigate = useNavigate();
     const [title, setTitle] = useState('');
     const [poster, setPoster] = useState('');
     const [trailerLink, setTrailerLink] = useState('');
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login');
+        }
+    }, [user, navigate]);
 
     if (!user) {
         navigate('/login');
@@ -21,6 +30,7 @@ function UserPage() {
         setUser(null);
         navigate('/login');
     };
+
 
     const handleAddMovie = async () => {
         try {
@@ -49,37 +59,51 @@ function UserPage() {
 
     return (
         <div className="user-page">
-            <h1>My Movie Storage</h1>
-            <div>
-                <label>Username:</label>
-                <input type="text" value={user.username} readOnly />
-            </div>
-            <button type="submit" onClick={handleLogout}>Logout</button>
+            <div className="user-container">
+                <div className='container'>
+                    <label className='label'>Username:</label>
+                    <input className='input' type='text' value={user.username} readOnly/>
+                </div>
+                <button type="submit" onClick={handleLogout}>Logout</button>
 
-            <h2>Add Movie</h2>
-            <div>
-                <label>Title:</label>
-                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+                <h2>Add Movie</h2>
+                <div className="container">
+                    <label className="label">Title:</label>
+                    <input className='input' type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                </div>
+                <div className="container">
+                    <label className="label">Poster:</label>
+                    <input className='input' type="text" value={poster} onChange={(e) => setPoster(e.target.value)}/>
+                </div>
+                <div className="container">
+                    <label className="label">Trailer Link:</label>
+                    <input className='input' type="text" value={trailerLink}
+                           onChange={(e) => setTrailerLink(e.target.value)}/>
+                </div>
+                <button type="submit" onClick={handleAddMovie}>Add Movie</button>
             </div>
-            <div>
-                <label>Poster:</label>
-                <input type="text" value={poster} onChange={(e) => setPoster(e.target.value)} />
-            </div>
-            <div>
-                <label>Trailer Link:</label>
-                <input type="text" value={trailerLink} onChange={(e) => setTrailerLink(e.target.value)} />
-            </div>
-            <button type="submit" onClick={handleAddMovie}>Add Movie</button>
 
             <h2>Movies</h2>
-            <div className="movie-list">
-                {movies.map(movie => (
-                    <div key={movie.imdbid} className="movie-item">
-                        <h2>{movie.title}</h2>
-                        <img src={movie.poster} alt={movie.title} />
-                        <button type="submit" onClick={() => handleDeleteMovie(movie.imdbid)}>
-                            Delete Movie
-                        </button>
+            <div className='movie-list'>
+                {movies.map((movie) => (
+                    <div key={movie.imdbid} className='movie-item'>
+                        <Link to={`/movie/${movie.imdbid}`}>
+                            <h2>{movie.title}</h2>
+                        </Link>
+                        <div className='movie-content'>
+                            <img src={movie.poster} alt={movie.title} className='movie-poster'/>
+                            <div className='icon-container'>
+                                <div className='icon' onClick={() => toggleFavorite(movie.imdbid)}>
+                                    <img src={movie.is_favorite ? goldStar : grayStar} alt='Favorite Icon'/>
+                                </div>
+                                <div className='icon' onClick={() => handleDeleteMovie(movie.imdbid)}>
+                                    <img src={basket} alt='Delete Movie'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='trailer-container'>
+                            <iframe src={movie.trailer_link} title={movie.title} allowFullScreen/>
+                        </div>
                     </div>
                 ))}
             </div>
